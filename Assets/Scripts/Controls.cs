cb7ba4a469a7fe593407865ad4bea4dff8664f4b
Claudio-Visentin <D16126698@mydit.ie>
2020-02-12 14:21:11 +0000

    Controls

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public float maxSpeed;
+    public float acceleration;
+    public float upFloat;
+    public float mileage = 10;
+    public Rigidbody2D rigid;
+    public float health;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        print(rigid.velocity.magnitude);
+        if (Input.GetKeyDown (KeyCode.W))
+        {
+            mileage -= 1;
+            rigid.AddForce(new Vector2 (0f, upFloat));
+        }
+
+        if (Input.GetKey (KeyCode.D))
+        {
+            if (rigid.velocity.magnitude < maxSpeed)
+            {
+                rigid.AddForce(new Vector2(acceleration, 0f));
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (rigid.velocity.magnitude < maxSpeed)
+            {
+                rigid.AddForce(new Vector2(-acceleration, 0f));
+            }
+        }
+    }
+}
