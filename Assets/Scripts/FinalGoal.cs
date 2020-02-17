a86294651663d5c89532569e0747d8a8c24017ab
Claudio-Visentin <D16126698@mydit.ie>
2020-02-17 16:29:15 +0000

    Upgrade System + End Goal Script

---
+++ Assets/Scripts/FinalGoal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+public class FinalGoal: MonoBehaviour
+{
+    public TextMeshProUGUI winnertext;
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("Player1"))
+        {
+            winnertext.gameObject.SetActive(true);
+            winnertext.text = "Player 1 Wins!";
+            Destroy(this);
+        }
+        if (other.gameObject.CompareTag("Player2"))
+        {
+            winnertext.gameObject.SetActive(true);
+            winnertext.text = "Player 2 Wins!";
+            Destroy(this);
+        }
+    }
+}
