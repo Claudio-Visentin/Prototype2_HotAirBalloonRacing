4a41a0d31485175233701af46fa208a30cffdeae
Claudio-Visentin <D16126698@mydit.ie>
2020-02-21 12:34:49 +0000

    out of camera respawn

---
+++ Assets/Scripts/OutofCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutofCamera : MonoBehaviour
+{
+    public Renderer P1;
+    public Renderer P2;
+    public Transform spanwer;
+    public void Update()
+    {
+        if (P1.isVisible == false)
+        {
+            P1.gameObject.transform.position = spanwer.position;
+        }
+        if (P2.isVisible == false)
+        {
+            P2.gameObject.transform.position = spanwer.position;
+        }
+    }
+}
